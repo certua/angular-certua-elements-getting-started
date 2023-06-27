f77bb30a3a013844a914cc9413bf595f026eceee
Vickie Allen <vickie@certua.io>
2023-06-27 13:15:55 +0100

    chore: base href

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,9 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
 import { importProvidersFrom } from '@angular/core';
 import { AppComponent } from './app/app.component';
 import { AppRoutingModule } from './app/app-routing.module';
 import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
 import { provideHttpClient } from '@angular/common/http';
 import { environment } from './environments/environment';
-declare var __webpack_public_path__: string;
-
-__webpack_public_path__ = 'angular';
 
 bootstrapApplication(AppComponent, {
   providers: [
