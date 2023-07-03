a549422cc7c42b3aac3e5a98517fa16a5c7e82e7
Vickie Allen <vickie@certua.io>
2023-07-03 17:13:47 +0100

    fix: sort out common inputs

--- src/app/insurance/insurance-common-inputs/common-inputs.component.ts
+++ src/app/insurance/insurance-common-inputs/common-inputs.component.ts
@@ -1,6 +1,11 @@
 import { RouterLink } from '@angular/router';
 import { CommonModule, NgIf } from '@angular/common';
-import { Component, CUSTOM_ELEMENTS_SCHEMA, OnInit } from '@angular/core';
+import {
+  Component,
+  CUSTOM_ELEMENTS_SCHEMA,
+  Input,
+  OnInit,
+} from '@angular/core';
 import { add, parseISO } from 'date-fns';
 
 @Component({
@@ -12,6 +17,7 @@ import { add, parseISO } from 'date-fns';
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class InsuranceCommonInputsComponent implements OnInit {
+  @Input() quoteAndBuy = false;
   config = {
     referrerId: 'xxxxx-xxxx-xxxx',
     basePath: 'angular/components/quote-and-buy',
