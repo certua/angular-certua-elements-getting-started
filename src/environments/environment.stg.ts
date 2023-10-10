90391dc32508a525ee0d17863a857c3ab442d428
Vickie Allen <vickie@certua.io>
2023-10-10 09:10:49 +0100

    chore: release v1.0.10-pre

--- src/environments/environment.stg.ts
+++ src/environments/environment.stg.ts
@@ -15,7 +15,7 @@ export const environment = {
       subTenantReference: '12499',
     },
     elementsURL:
-      'https://cdn.certua.io/ux-micro-frontends/open-banking-elements/stg/v1.0.9-pre',
+      'https://cdn.certua.io/ux-micro-frontends/open-banking-elements/stg/v1.0.10-pre',
   },
   insurance: {
     quoteAndBuyURL:
