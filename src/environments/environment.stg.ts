20a682206280f98e66af13a769fabc8e8ab6e53f
Greg <greg@certua.io>
2025-07-18 13:56:36 +0100

    chore: update open banking elements version

--- src/environments/environment.stg.ts
+++ src/environments/environment.stg.ts
@@ -15,7 +15,7 @@ export const environment = {
       subTenantReference: '12499',
     },
     elementsURL:
-      'https://cdn.certua.io/ux-micro-frontends/open-banking-elements/stg/v1.0.21-pre',
+      'https://cdn.certua.io/ux-micro-frontends/open-banking-elements/stg/v1.0.22-pre',
   },
   insurance: {
     quoteAndBuyURL:
